cff5be12fb23bb26e6bbe3cf011b25677b0b5528
Jim Van Fleet <jim@jimvanfleet.com>
2022-08-11 17:41:30 -0400

    They should be in default roles location now

--- bin/eagle_job.sh
+++ bin/eagle_job.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 az login --identity
-export DEFAULT_ROLES_PATH=/workspace/directory/etc/ansible/roles
 export ANSIBLE_VAULT_PASSWORD_FILE=/workspace/ansible-vault-directory/vaultpw 
 ansible-playbook -e "rna_file=../rna.yml" -v $PARAM_EAGLE_JOB.yml
