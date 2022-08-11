29d286f583adef9c14c4c6cf1283e9926f1b9ea0
Jim Van Fleet <jim@jimvanfleet.com>
2022-08-11 17:38:21 -0400

    Better guess at RNA location

--- bin/eagle_job.sh
+++ bin/eagle_job.sh
@@ -2,4 +2,4 @@
 az login --identity
 export DEFAULT_ROLES_PATH=/workspace/directory/etc/ansible/roles
 export ANSIBLE_VAULT_PASSWORD_FILE=/workspace/ansible-vault-directory/vaultpw 
-ansible-playbook -e "rna_file=etc/rna.yml" -v $PARAM_EAGLE_JOB.yml
+ansible-playbook -e "rna_file=../rna.yml" -v $PARAM_EAGLE_JOB.yml
