b94b0e8aa1142449cc8927ce9b8bfcd4c5353eef
Jim Van Fleet <jim.van.fleet@levvel.io>
2022-08-12 08:59:52 -0400

    Eagle job now needs to install roles

--- bin/eagle_job.sh
+++ bin/eagle_job.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ansible-galaxy install -r roles/requirements.yml
 az login --identity
+
 export ANSIBLE_VAULT_PASSWORD_FILE=/workspace/ansible-vault-directory/vaultpw 
 ansible-playbook -e "rna_file=../rna.yml" -v $PARAM_EAGLE_JOB.yml
