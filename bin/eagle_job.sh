5050f0c0ee4b8bb7a4118095b4d454dacc52200f
Jim Van Fleet <jim.van.fleet@levvel.io>
2022-08-23 18:21:08 -0400

    Preparing eagle job to allow SP passthrough

--- bin/eagle_job.sh
+++ bin/eagle_job.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 ansible-galaxy install -r roles/requirements.yml
-az login --identity
+if ${WORKSPACE_AZURE_DIRECTORY_BOUND}
+then
+  echo 'Logging in with SP'
+  source <(grep = "${WORKSPACE_AZURE_DIRECTORY_PATH}"/credentials)
+  az login --service-principal --username $client_id --password $secret --tenant $tenant
+else
+  echo 'Logging in with pod identity'
+  az login --identity
+fi
 
 export ANSIBLE_VAULT_PASSWORD_FILE=/workspace/ansible-vault-directory/vaultpw 
 ansible-playbook -e "rna_file=../rna.yml" -v $PARAM_EAGLE_JOB.yml
